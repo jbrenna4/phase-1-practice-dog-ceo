b6940f27575f0b0091416c2c79a94482afe35df0
Jim Brennan <jim.j.brennan@gmail.com>
2023-01-17 21:30:39 -0500

    finished the initial fetch request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+//fetch, parse and add image elements
+
+//const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+//fetch function
+function firstFetch(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(dogData => dogData.message.forEach(dog => renderDogImage(dog)))
+};
+
+firstFetch();
+
+//render function - 
+function renderDogImage(dog){
+    const dogImage = document.createElement('img');
+    console.log(dogImage);
+//    dogImage.src = dog;
+    }
+renderDogImage();
